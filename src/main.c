315cb186032e5a070b9c2fe206fc29f2ef274bab
Ruslan Bukin <br@bsdpad.com>
2019-12-15 22:53:30 +0000

    Update mdepx.

--- src/main.c
+++ src/main.c
@@ -43,11 +43,12 @@
 #include <arm/arm/nvic.h>
 #include <arm/nordicsemi/nrf9160.h>
 
-struct uarte_softc uarte_sc;
 struct arm_nvic_softc nvic_sc;
-struct spu_softc spu_sc;
-struct power_softc power_sc;
-struct timer_softc timer0_sc;
+
+struct nrf_uarte_softc uarte_sc;
+struct nrf_spu_softc spu_sc;
+struct nrf_power_softc power_sc;
+struct nrf_timer_softc timer0_sc;
 
 #define	UART_PIN_TX	29
 #define	UART_PIN_RX	28
@@ -88,8 +89,8 @@ int buffer_fill;
 int ready_to_send;
 
 static const struct nvic_intr_entry intr_map[NVIC_NINTRS] = {
-	[ID_UARTE0] = { uarte_intr, &uarte_sc },
-	[ID_TIMER0] = { timer_intr, &timer0_sc },
+	[ID_UARTE0] = { nrf_uarte_intr, &uarte_sc },
+	[ID_TIMER0] = { nrf_timer_intr, &timer0_sc },
 	[ID_EGU1] = { rpc_proxy_intr, NULL },
 	[ID_EGU2] = { trace_proxy_intr, NULL },
 	[ID_IPC] = { ipc_proxy_intr, NULL },
@@ -119,14 +120,14 @@ ipc_proxy_intr(void *arg, struct trapframe *tf, int irq)
 static void
 uart_putchar(int c, void *arg)
 {
-	struct uarte_softc *sc;
+	struct nrf_uarte_softc *sc;
  
 	sc = arg;
  
 	if (c == '\n')
-		uarte_putc(sc, '\r');
+		nrf_uarte_putc(sc, '\r');
 
-	uarte_putc(sc, c);
+	nrf_uarte_putc(sc, c);
 }
 
 void
@@ -361,23 +362,23 @@ int
 app_init(void)
 {
 
-	uarte_init(&uarte_sc, BASE_UARTE0,
+	nrf_uarte_init(&uarte_sc, BASE_UARTE0,
 	    UART_PIN_TX, UART_PIN_RX, UART_BAUDRATE);
-	console_register(uart_putchar, (void *)&uarte_sc);
-	uarte_register_callback(&uarte_sc, nrf_input, NULL);
+	mdx_console_register(uart_putchar, (void *)&uarte_sc);
+	nrf_uarte_register_callback(&uarte_sc, nrf_input, NULL);
 
 	printf("mdepx initialized\n");
 
-	fl_init();
-	fl_add_region(0x20030000, 0x10000);
+	mdx_fl_init();
+	mdx_fl_add_region(0x20030000, 0x10000);
 
-	power_init(&power_sc, BASE_POWER);
+	nrf_power_init(&power_sc, BASE_POWER);
 
 	arm_nvic_init(&nvic_sc, BASE_SCS);
 	arm_nvic_install_intr_map(&nvic_sc, intr_map);
 	arm_nvic_set_prio(&nvic_sc, ID_IPC, 6);
 
-	timer_init(&timer0_sc, BASE_TIMER0);
+	nrf_timer_init(&timer0_sc, BASE_TIMER0);
 	arm_nvic_enable_intr(&nvic_sc, ID_TIMER0);
 	arm_nvic_enable_intr(&nvic_sc, ID_UARTE0);
 
