407e5adfb937f8eb9dd77ea4647e89b53bd54b6f
Ruslan Bukin <br@bsdpad.com>
2021-02-21 21:55:59 +0000

    Update copyright.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2018-2020 Ruslan Bukin <br@bsdpad.com>
+ * Copyright (c) 2018-2021 Ruslan Bukin <br@bsdpad.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
