101a17801ce9d256b35c20cd7cbc6e3218e609a1
Ruslan Bukin <br@bsdpad.com>
2021-07-24 19:47:17 +0100

    Update copyright.

--- src/nrf_modem.c
+++ src/nrf_modem.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2019-2020 Ruslan Bukin <br@bsdpad.com>
+ * Copyright (c) 2019-2021 Ruslan Bukin <br@bsdpad.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
